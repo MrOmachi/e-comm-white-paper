fc7577c98fc64e43249d7694b9f84c3f6545d7b8
Omachi John <omachi.john@.com>
2022-07-26 20:30:46 +0100

    Personalise Structure content

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,8 @@
 let hamburger = document.getElementById('hamburger');
 let desktopList = document.getElementById('desktopList');
 let closeX = document.getElementById('closeX');
+let speaker3 = document.getElementById('speaker3');
+let btn = document.getElementById('btn');
 
 [hamburger, closeX, desktopList].forEach((elem) => {
   elem.addEventListener('click', function () {
@@ -8,4 +10,9 @@ let closeX = document.getElementById('closeX');
     closeX.classList.toggle('showUlItems');
     hamburger.classList.toggle('hide');
   });
+});
+
+btn.addEventListener('click', () => {
+  speaker3.classList.toggle('show');
+  btn.classList.toggle('hide');
 });
